aa0cc691e336a1db0ea9c2fcc7bf16f658d78a77
Dmitry Artemev <dorvakd9@gmail.com>
2022-01-20 17:45:13 +0300

    Дополнительная сцена - выбора типа задачи

---
+++ To-Do Manager/TaskTypeController.swift
@@ -0,0 +1,12 @@
+//
+//  TaskTypeController.swift
+//  To-Do Manager
+//
+//  Created by Дмитрий Артемьев on 19.01.2022.
+//
+
+import UIKit
+
+class TaskTypeController: UITableViewController {
+
+}
